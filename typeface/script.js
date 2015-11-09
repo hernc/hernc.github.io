ea62a9644b9790c487276761af474b60eb547671
hernc <hernc820@newschool.edu>
2015-11-09 18:06:09 -0500

    added toggle function to link

--- typeface/script.js
+++ typeface/script.js
@@ -1,3 +1,58 @@
+//RESTART ANIMATION ON CLICK
+
+
 $(".letter-a").click(function(){
-	$(".letter-a").toggleClass(".animate")
+	$(".la").toggleClass("animateLa");
+	$(".ca").toggleClass("animateCa");
+	$(".ra").toggleClass("animateRa");
+
 });
+
+//ATTEMPT 2
+
+
+//$(".home").click(function(){
+//    $(".pson").toggle();
+//});
+//
+//$(".song1").click(function(){
+//		$("nav li").removeClass("phrase");
+//    $(".riverroad").toggle();
+//
+//});
+
+
+//ATTEMPT 3
+
+$(".riverroad").hide();
+
+    $(".home, .song1").bind("click", function () {
+
+      $(".pson, .riverroad").hide();
+
+      if ($(this).attr("class") == "home")
+      {
+        $(".pson").show();
+      }
+      else
+      {
+        $(".riverroad").show();
+				preventDefault();
+      }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
