c503f618ff7e91d200e5c834a4ecd43f2a042639
hernc <hernc820@newschool.edu>
2015-11-10 22:14:24 -0500

    added song to river road

--- typeface/script.js
+++ typeface/script.js
@@ -1,30 +1,21 @@
+
 //RESTART ANIMATION ON CLICK
 
 
-$(".letter-a").click(function(){
+$(".letter-a").click(function () {
 	$(".la").toggleClass("animateLa");
 	$(".ca").toggleClass("animateCa");
 	$(".ra").toggleClass("animateRa");
 
 });
 
-//ATTEMPT 2
 
 
-//$(".home").click(function(){
-//    $(".pson").toggle();
-//});
-//
-//$(".song1").click(function(){
-//		$("nav li").removeClass("phrase");
-//    $(".riverroad").toggle();
-//
-//});
-
-
-//ATTEMPT 3
+//HIDE & SHOW PHRASES AND SONGS
 
 $(".riverroad").hide();
+$(".pson").show();
+stopRiver();
 
     $(".home, .song1").bind("click", function () {
 
@@ -33,17 +24,52 @@ $(".riverroad").hide();
       if ($(this).attr("class") == "home")
       {
         $(".pson").show();
+				startHome();
+				stopRiver();
+
       }
       else
       {
         $(".riverroad").show();
+				stopHome();
+				startRiver();
+
 				preventDefault();
       }
-    });
+	});
 
 
+// HOME SOUND CODE
+
+$(".homesong").prop('volume', 0.6);
+
+function stopHome(){
+  //pause playing
+  $(".homesong").trigger('pause');
+  //set play time to 0
+  $(".homesong").prop("currentTime",0);
+}
 
 
+function startHome(){
+  //start playing
+  $(".homesong").trigger('play');
+}
+
+// RIVERROAD SOUND CODE
+
+function stopRiver(){
+  //pause playing
+  $(".river1").trigger('pause');
+  //set play time to 0
+  $(".river1").prop("currentTime",0);
+}
+
+
+function startRiver(){
+  //start playing
+  $(".river1").trigger('play');
+}
 
 
 
