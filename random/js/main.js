e42b0bbe4f84456c9ca7905ad12ffcd897f50a45
hernc <hernc820@newschool.edu>
2015-11-20 01:31:32 -0500

    typeface site updated

--- random/js/main.js
+++ random/js/main.js
@@ -1,1 +1,52 @@
-$(".button").html("let's Go")
+//$(".letter-a").click(function () {
+//	$(".la").toggleClass("animateLa");
+//	$(".ca").toggleClass("animateCa");
+//	$(".ra").toggleClass("animateRa");
+//
+//});
+
+
+
+$(".letter-a").click(function(){
+    $(".la").animate({  textIndent: 15 }, {
+    step: function(now,fx) {
+      $(this).css('-webkit-transform','rotate('+now+'deg)');
+    },
+    duration:'500'
+},'linear');
+	$(".ra").animate({  textIndent: -15 }, {
+    step: function(now,fx) {
+      $(this).css('-webkit-transform','rotate('+now+'deg)');
+    },
+    duration:'500'
+},'linear');
+	$(".ca").animate({  textIndent: -90 }, {
+    step: function(now,fx) {
+      $(this).css('-webkit-transform','rotate('+now+'deg)');
+    },
+    duration:'500'
+},'linear');
+});
+
+
+
+//TRANSFORM RAMDOMLY
+	//create an array of properties to animate randomly
+var properties = ["height", "width"]
+	//on click animate letter using random values
+
+	//duplicate letter with different animation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- typeface/script.js
+++ typeface/script.js
@@ -1,3 +1,17 @@
+//STICKY NAV
+
+$(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >=1) {
+            $('.nav').addClass('stickytop');
+
+        }
+        else {
+            $('.nav').removeClass('stickytop');
+						$('.wrapper').css('padding-top', '0');
+        }
+    });
+});
 
 //RESTART ANIMATION ON CLICK
 
@@ -13,33 +27,34 @@ $(".letter-a").click(function () {
 
 //HIDE & SHOW PHRASES AND SONGS
 
-$(".riverroad").hide();
-$(".pson").show();
-stopRiver();
-
-    $(".home, .song1").bind("click", function () {
-
-      $(".pson, .riverroad").hide();
-
-      if ($(this).attr("class") == "home")
-      {
-        $(".pson").show();
-				startHome();
-				stopRiver();
-
-      }
-      else
-      {
-        $(".riverroad").show();
-				stopHome();
-				startRiver();
-
-				preventDefault();
-      }
-	});
+//$(".riverroad").hide();
+//$(".pson").show();
+//stopRiver();
+//
+//    $(".home, .song1").bind("click", function () {
+//
+//      $(".pson, .riverroad").hide();
+//
+//      if ($(this).attr("class") == "home")
+//      {
+//        $(".pson").show();
+//				startHome();
+//				stopRiver();
+//
+//      }
+//      else
+//      {
+//        $(".riverroad").show();
+//				stopHome();
+//				startRiver();
+//
+//				preventDefault();
+//      }
+//	});
 
 
 // HOME SOUND CODE
+
 
 $(".homesong").prop('volume', 0.6);
 
@@ -56,23 +71,66 @@ function startHome(){
   $(".homesong").trigger('play');
 }
 
-// RIVERROAD SOUND CODE
+// RIVERROAD HIDE/SHOW SOUND CODE
 
-function stopRiver(){
+//function stopRiver(){
   //pause playing
-  $(".river1").trigger('pause');
+//  $(".river1").trigger('pause');
   //set play time to 0
-  $(".river1").prop("currentTime",0);
+//  $(".river1").prop("currentTime",0);
+//}
+
+
+//function startRiver(){
+//  //start playing
+//  $(".river1").trigger('play');
+//}
+
+
+
+$('.v-button').click(function(event) {
+	event.preventDefault();
+
+	var target   = $(this).data('target'),
+			y_offset = $(target).offset().top;
+
+	$('body, html').animate({ scrollTop: y_offset - 20 });
+})
+
+// RIVERROAD SCROLL SOUND CODE
+$(window).scroll(function() {
+  choose_song_to_play();
+});
+
+
+function choose_song_to_play() {
+	var y = $(window).scrollTop();
+	//starting position
+	if (y < 789) {
+		$("#river1").trigger('play');
+		$("#river2").trigger('pause');
+		$("#river3").trigger('pause');
+		//second position
+	} else if (y >= 790 && y < 1509) {
+		$("#river2").trigger('play');
+		$("#river1").trigger('pause');
+		$("#river3").trigger('pause');
+		//third position
+	} else if (y >= 1510 && y < 2500) {
+		$("#river3").trigger('play');
+		$("#river2").trigger('pause');
+		$("#river1").trigger('pause');
+	}
 }
 
-
-function startRiver(){
-  //start playing
-  $(".river1").trigger('play');
-}
-
-
-
+//var $window = $(window);
+//
+//$(window).on('scroll', function() {
+//    $topOffset = $(this).scrollTop();
+//
+//    console.log($topOffset);
+//
+//});
 
 
 
