2cf7140d795086a51cb3591e9c1a7c37643d80ea
hernc <hernc820@newschool.edu>
2015-12-08 15:18:30 -0500

    worked on perspective

--- illusion/script.js
+++ illusion/script.js
@@ -3,4 +3,10 @@
 $('.lettera').click(function(){
 	$('.lettera').not(this).find('.innerIllusion').hide();
 	$(this).find('.innerIllusion').toggle();
+	$('lettera').fadeOut("fast",function(){
+      $(this).css('z-index','10');
+		});
+
 });
+
+
