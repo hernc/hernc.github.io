08b67beaa02d4a5e0b2bcd47dc0ec18feaaa23c2
hernc <hernc820@newschool.edu>
2015-12-13 00:16:28 -0500

    transition to letters

--- illusion/script.js
+++ illusion/script.js
@@ -1,12 +1,22 @@
 //LETTER INTERACTION
 
-$('.lettera').click(function(){
-	$('.lettera').not(this).find('.innerIllusion').hide();
-	$(this).find('.innerIllusion').toggle();
-	$('lettera').fadeOut("fast",function(){
-      $(this).css('z-index','10');
-		});
+//$('.lettera').click(function(){
+//	$('.lettera').not(this).find('.innerIllusion').hide();
+//	$(this).find('.innerIllusion').toggle();
+//	$('lettera').fadeOut("slow",function(){
+//      $(this).css('scale','5');
+//		});
+//
+//});
 
-});
+//$(".lettera").click(function() {
+//        var el = $(this);
+//        el.before( el.clone(true) ).remove();
+//    });
 
 
+
+$(".lettera").click(function() {
+		$(".lettera").toggleClass("hidden");
+		$(".innerIllusion").toggleClass("innerIllusionhidden");
+	});
